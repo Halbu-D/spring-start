217948a8e415bdafffed2ae8b859e0e713d4dde9
Halbu <anos1021@gmail.com>
2024-07-18 22:56:17 +0900

    db연동 설정

--- src/main/java/com/halbu/spring_start/SpringConfig.java
+++ src/main/java/com/halbu/spring_start/SpringConfig.java
@@ -1,14 +1,23 @@
 package com.halbu.spring_start;
 
+import com.halbu.spring_start.repository.JdbcMemberRepository;
 import com.halbu.spring_start.repository.MemberRepository;
 import com.halbu.spring_start.repository.MemoryMemberRepository;
 import com.halbu.spring_start.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
+    private final DataSource dataSource;
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -16,6 +25,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
